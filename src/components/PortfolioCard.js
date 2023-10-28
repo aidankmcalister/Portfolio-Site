52e98e841bb8c7750a5f4997a376dd5a8a1f3fd3
Aidan <aidankmcalister@gmail.com>
2023-10-27 20:40:18 -0500

    accordion

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { PROJECTS } from "../app/shared/PROJECTS";
-import { tag } from "./portfolioPage/tags";
-// import LazyLoad from "react-lazy-load";
+
+import ProjectAccordion from "./ProjectAccordion";
 
 const PortfolioCard = () => {
   const [renderedProjects, setRenderedProjects] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       if (currentIndex < PROJECTS.length) {
@@ -20,47 +21,14 @@ const PortfolioCard = () => {
   }, [currentIndex]);
 
   return (
-    <div className="md:h-52 bg-opacity-70 bg-slate-800  m-4 rounded-lg p-3 flex flex-col text-white max-w-lg animate-fade-up opacity-80 font-rubik">
+    <div className="md:h-52 bg-opacity-70 bg-slate-800 m-4 rounded-lg p-3 flex flex-col text-white max-w-lg animate-fade-up opacity-80 font-rubik">
       <div className="flex justify-between flex-row">
         <h1 className="text-2xl text-slate-300">Portfolio</h1>
-
-        {/* <div className="flex flex-row items-center">
-          <p className="mx-1 font-sometype text-slate-400">DESC.</p>
-          <label class="switch">
-            <input type="checkbox" />
-            <span class="slider"></span>
-          </label>
-        </div> */}
       </div>
       <hr className="w-full my-2 text-lg mt-1" />
       <ul className="grid md:grid-cols-2 md:gap-3 gap-1.5 grid-cols-1">
         {renderedProjects.map((project) => (
-          <li
-            key={project.id}
-            className="flex-col animate-fade-left p-1.5 flex rounded-md font-sometype shadow-sm shadow-custom-light-blue-shadow bg-main-secondary-blue">
-            {project.img && (
-              <img
-                src={project.img}
-                alt={project.name}
-                className="rounded-md"
-              />
-            )}
-            <div>
-              {/* <div className="flex justify-between"> */}
-
-              <div className=" font-rubik text-xl my-1">{project.name}</div>
-
-              {/* <a
-                  style={{ boxShadow: "0px 0px .25rem" }}
-                  className="shadow-slate-300 flex items-center rounded-full text-sm px-2 my-1 bg-opacity-70 border-slate-300 text-slate-300 border"
-                  href={`/project/${project.id}`}>
-                  Find Out!
-                </a>
-              </div> */}
-
-              {tag(project.lang)}
-            </div>
-          </li>
+          <ProjectAccordion project={project} />
         ))}
       </ul>
     </div>
---
+++ src/components/ProjectAccordion.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import { tag } from "./portfolioPage/tags";
+
+function Icon({ isOpen }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`h-7 w-7 mr-4 transition-transform transform ${
+        isOpen ? "rotate-180" : ""
+      }`}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
+
+const ProjectAccordion = ({ project }) => {
+  const [open, setOpen] = useState(false);
+  const [contentVisible, setContentVisible] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+
+    if (!open) {
+      setContentVisible(true);
+    } else {
+      setTimeout(() => {
+        setContentVisible(false);
+      }, 100);
+    }
+  };
+
+  return (
+    <Accordion
+      className="my-0.5 pt-1.5 flex-col animate-fade-left px-1.5 flex rounded-md font-sometype shadow-sm shadow-custom-light-blue-shadow bg-main-secondary-blue"
+      open={open}
+      icon={<Icon isOpen={open} />}>
+      {project.img && (
+        <img src={project.img} alt={project.name} className="rounded-md" />
+      )}
+      <AccordionHeader onClick={handleOpen}>
+        <div className="flex items-center">
+          <div className="flex flex-col items-start ml-2">
+            <div className="font-rubik text-xl my-1">{project.name}</div>
+            <div className="flex-wrap flex">{tag(project.lang)}</div>
+          </div>
+        </div>
+      </AccordionHeader>
+      <AccordionBody>
+        <div
+          className={`bg-slate-800 p-1 rounded-lg ${
+            contentVisible ? "block" : "hidden"
+          }`}>
+          {project.description}
+        </div>
+      </AccordionBody>
+    </Accordion>
+  );
+};
+
+export default ProjectAccordion;
